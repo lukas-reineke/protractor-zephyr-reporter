39cb280b3a0a17fe5bd273508b77556ca14b1954
Lukas Reineke <lukas.reineke@interone.de>
2018-01-06 16:57:53 +0100

    Added some unit tests

--- spec/zephyr-reporter.spec.js
+++ spec/zephyr-reporter.spec.js
@@ -40,20 +40,22 @@ describe(`ZephyrReporter`, () => {
         expect(onCompleteDefer.fulfill).toHaveBeenCalled();
     });
 
+
     it(`should handle missing required options`, () => {
 
         spyOn(onPrepareDefer, 'fulfill');
         spyOn(onCompleteDefer, 'fulfill');
 
         const zephyrReporter = ZephyrReporter({
-            zapiUrl: 'mock',
-            jiraUrl: 'mock'
+            zapiUrl: 'http://zapi-url.com',
+            jiraUrl: 'http://jira-url.com'
         }, onPrepareDefer, onCompleteDefer, browser);
 
         expect(zephyrReporter).toBeUndefined();
         expect(onPrepareDefer.fulfill).toHaveBeenCalled();
         expect(onCompleteDefer.fulfill).toHaveBeenCalled();
     });
+
 
     it(`should not do anything if it is disabled`, () => {
 
@@ -62,8 +64,8 @@ describe(`ZephyrReporter`, () => {
 
         const zephyrReporter = ZephyrReporter({
             projectId: 'mock',
-            zapiUrl: 'mock',
-            jiraUrl: 'mock',
+            zapiUrl: 'http://zapi-url.com',
+            jiraUrl: 'http://jira-url.com',
             disabled: true
         }, onPrepareDefer, onCompleteDefer, browser);
 
@@ -72,34 +74,179 @@ describe(`ZephyrReporter`, () => {
         expect(onCompleteDefer.fulfill).toHaveBeenCalled();
     });
 
+
     it(`should return a function if all required options are met`, (done) => {
 
         spyOn(onPrepareDefer, 'fulfill');
 
-        const nock = Nock('http://mock.com')
+        let result;
+        const dateRegex = /\d\/\w{3}\/\d/;
+
+        const nock = Nock('http://zapi-url.com')
             .defaultReplyHeaders({
                 'Content-Type': 'application/json'
             })
             .post('/cycle')
-            .reply(() => {
+            .reply((path, res) => {
+                result = res;
                 return { id: '1212' };
             });
 
         const zephyrReporter = ZephyrReporter({
             projectId: 'mock',
-            zapiUrl: 'http://mock.com',
-            jiraUrl: 'mock'
+            zapiUrl: 'http://zapi-url.com',
+            jiraUrl: 'http://jira-url.com'
         }, onPrepareDefer, onCompleteDefer, browser);
 
         nock.on('replied', () => {
             setTimeout(() => {
                 expect(zephyrReporter).toBeDefined();
                 expect(onPrepareDefer.fulfill).toHaveBeenCalled();
+                expect(result.name).toEqual('mock');
+                expect(result.projectId).toEqual('mock');
+                expect(result.versionId).toEqual(-1);
+                expect(result.sprintId).toEqual(-1);
+                expect(dateRegex.test(result.startDate)).toBeTruthy();
+                expect(dateRegex.test(result.endDate)).toBeTruthy();
                 done();
             });
         });
 
     });
 
+
+    describe(`suiteStarted()`, () => {
+
+        it(`should do nothing if the reporter is disabled`, (done) => {
+
+            browser = {
+                getProcessedConfig: () => {
+                    return new Promise((resolve, reject) => {
+                        reject('force error');
+                    });
+                }
+            };
+
+            const zephyrReporter = ZephyrReporter({
+                projectId: 'mock',
+                zapiUrl: 'http://zapi-url.com',
+                jiraUrl: 'http://jira-url.com'
+            }, onPrepareDefer, onCompleteDefer, browser);
+
+            setTimeout(() => {
+                expect(zephyrReporter.suiteStarted()).toBeUndefined();
+                done();
+            });
+
+        });
+
+
+        it(`should create an execution`, (done) => {
+
+            let result;
+
+            const nockCycle = Nock('http://zapi-url.com')
+                .defaultReplyHeaders({
+                    'Content-Type': 'application/json'
+                })
+                .post('/cycle')
+                .reply(() => {
+                    return { id: '1212' };
+                });
+
+            const nockExecution = Nock('http://zapi-url.com')
+                .defaultReplyHeaders({
+                    'Content-Type': 'application/json'
+                })
+                .post('/execution')
+                .reply((path, res) => {
+                    result = res
+                    return { id: '1212' };
+                });
+
+            const zephyrReporter = ZephyrReporter({
+                projectId: 'mock',
+                zapiUrl: 'http://zapi-url.com',
+                jiraUrl: 'http://jira-url.com'
+            }, onPrepareDefer, onCompleteDefer, browser);
+
+            nockCycle.on('replied', () => {
+                setTimeout(() => {
+                    zephyrReporter.suiteStarted({
+                        description: 'mock@123'
+                    })
+                });
+            });
+
+            nockExecution.on('replied', () => {
+                setTimeout(() => {
+                    expect(result).toEqual({
+                        cycleId: '1212',
+                        issueId: '123',
+                        projectId: 'mock'
+                    });
+                    done();
+                });
+            });
+
+        });
+
+    });
+
+
+    describe(`specStarted()`, () => {
+
+        it(`should do nothing if the reporter is disabled`, (done) => {
+
+            browser = {
+                getProcessedConfig: () => {
+                    return new Promise((resolve, reject) => {
+                        reject('force error');
+                    });
+                }
+            };
+
+            const zephyrReporter = ZephyrReporter({
+                projectId: 'mock',
+                zapiUrl: 'http://zapi-url.com',
+                jiraUrl: 'http://jira-url.com'
+            }, onPrepareDefer, onCompleteDefer, browser);
+
+            setTimeout(() => {
+                expect(zephyrReporter.specStarted()).toBeUndefined();
+                done();
+            });
+
+        });
+
+    });
+
+
+    describe(`specDone()`, () => {
+
+        it(`should do nothing if the reporter is disabled`, (done) => {
+
+            browser = {
+                getProcessedConfig: () => {
+                    return new Promise((resolve, reject) => {
+                        reject('force error');
+                    });
+                }
+            };
+
+            const zephyrReporter = ZephyrReporter({
+                projectId: 'mock',
+                zapiUrl: 'http://zapi-url.com',
+                jiraUrl: 'http://jira-url.com'
+            }, onPrepareDefer, onCompleteDefer, browser);
+
+            setTimeout(() => {
+                expect(zephyrReporter.specDone()).toBeUndefined();
+                done();
+            });
+
+        });
+
+    });
 });
 
